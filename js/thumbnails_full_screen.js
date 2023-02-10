029cd2531f5c99f884b6bfe60a6a8f4db3854e53
@nadezhda.fedotova <ub-326@ub-326s-MacBook-Pro.local>
2023-02-10 16:34:21 +0200

    update

--- js/thumbnails_full_screen.js
+++ js/thumbnails_full_screen.js
@@ -1,4 +1,4 @@
-const onEscapeKey = (evt) => evt.key === 'Escape';
+import { onEscapeKey } from './utils.js';
 const body = document.querySelector('body');
 const pictureCancelPopup = document.querySelector('#picture-cancel');
 const bigPicture = document.querySelector('.big-picture');
@@ -10,43 +10,44 @@ const likesCount = bigPicture.querySelector('.likes-count');
 const socialComment = document.querySelector('.social__comment');
 const fragment = new DocumentFragment();
 
-function comment_render(data) {
-  socialComments.innerHTML = '';
-  data.forEach(element => {
-    const comment = socialComment.cloneNode(true);
-    comment.querySelector('.social__picture').src = element.avatar;
-    comment.querySelector('.social__picture').alt = element.name;
-    comment.querySelector('.social__text').textContent = element.message;
-    fragment.appendChild(comment);
-  });
-  socialComments.appendChild(fragment);
+function comment_render (data) {
+    socialComments.innerHTML = '';
+    data.forEach(element => {
+        const comment = socialComment.cloneNode(true);
+        comment.querySelector('.social__picture').src = element.avatar;
+        comment.querySelector('.social__picture').alt = element.name;
+        comment.querySelector('.social__text').textContent = element.message;
+        fragment.appendChild(comment);
+    });
+    socialComments.appendChild(fragment);
 }
 
 const closePopup = (evt) => {
-  if (onEscapeKey(evt)) {
-    evt.preventDefault();
-    closePopupPhoto();
-  }
+    if (onEscapeKey(evt)) {
+        evt.preventDefault();
+        closePopupPhoto();
+    }
 };
 
-function closePopupPhoto() {
-  bigPicture.classList.add('hidden');
-  body.classList.remove('modal-open');
-  document.addEventListener('keydown', closePopup);
+function closePopupPhoto () {
+    bigPicture.classList.add('hidden');
+    body.classList.remove('modal-open');
+    document.addEventListener('keydown', closePopup);
 }
 
-function thumbnailsFullScreen(data) {
-  bigPicture.classList.remove('hidden');
-  body.classList.add('modal-open');
+function thumbnailsFullScreen (data) {
+    bigPicture.classList.remove('hidden');
+    body.classList.add('modal-open');
 
-  bigPictureImg.src = data.url;
-  commentsCount.textContent = data.comments.length;
-  likesCount.textContent = data.likes;
-  socialCaption.textContent = data.description;
-  comment_render(data.comments);
+    bigPictureImg.src = data.url;
+    commentsCount.textContent = data.comments.length;
+    likesCount.textContent = data.likes;
+    socialCaption.textContent = data.description;
+    comment_render(data.comments);
 
-  document.addEventListener('keydown', closePopup);
+    document.addEventListener('keydown', closePopup);
 }
+
 pictureCancelPopup.addEventListener('click', closePopupPhoto);
 
-export {thumbnailsFullScreen};
+export { thumbnailsFullScreen, body };
